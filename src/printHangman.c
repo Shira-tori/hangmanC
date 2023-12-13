87bf6c64a1cc02dd054725a8020a12dcd14eda83
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2023-12-13 23:49:56 +0800

    moved source files to src

---
+++ src/printHangman.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+
+void printHangman(int *wrongs){
+	switch(*wrongs){
+		case 0:
+			printf("\t___________\n");
+			printf("\t|         |\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			break;
+		case 1:
+			printf("\t___________\n");
+			printf("\t|         |\n");
+			printf("\t|         O\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			break;
+		case 2:
+			printf("\t___________\n");
+			printf("\t|         |\n");
+			printf("\t|         O\n");
+			printf("\t|         |\n");
+			printf("\t|         |\n");
+			printf("\t|         |\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			break;
+		case 3:
+			printf("\t___________\n");
+			printf("\t|         |\n");
+			printf("\t|         O\n");
+			printf("\t|        /|\n");
+			printf("\t|       / |\n");
+			printf("\t|         |\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			break;
+		case 4:
+			printf("\t___________\n");
+			printf("\t|         |\n");
+			printf("\t|         O\n");
+			printf("\t|        /|\\\n");
+			printf("\t|       / | \\\n");
+			printf("\t|         |\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			printf("\t|\n");
+			break;
+		case 5:
+			printf("\t___________\n");
+			printf("\t|         |\n");
+			printf("\t|         O\n");
+			printf("\t|        /|\\\n");
+			printf("\t|       / | \\\n");
+			printf("\t|         |\n");
+			printf("\t|        / \n");
+			printf("\t|       /   \n");
+			printf("\t|\n");
+			break;
+		case 6:
+			printf("\t___________\n");
+			printf("\t|         |\n");
+			printf("\t|         O\n");
+			printf("\t|        /|\\\n");
+			printf("\t|       / | \\\n");
+			printf("\t|         |\n");
+			printf("\t|        / \\\n");
+			printf("\t|       /   \\\n");
+			printf("\t|\n");
+			break;
+	}
+}
